28ecec38501d1f8deb5daba34addb50830d61413
Surafel <surafelmoges01@gmail.com>
2025-05-29 09:57:11 +0100

    Wow ! it is my first React componet  i create

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 const containor = createRoot(document.getElementById("root"))
@@ -8,5 +10,35 @@ const Name = <h1>You are the best prograammer i ever seen before</h1>
 
 containor.render(
   Name
-)
+)   
+ 
 
+
+function myfirstReactComponet() {
+  return (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">MySite</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+);
+}
+containor.render(
+  createElement(myfirstReactComponet)
+)
