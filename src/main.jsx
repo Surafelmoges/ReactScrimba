6be68baedea4644b3449e2193a8e3d6a3ecbd8d4
Surafel <surafelmoges01@gmail.com>
2025-05-30 08:48:23 +0100

    Progress

--- src/main.jsx
+++ src/main.jsx
@@ -3,56 +3,42 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import "./App.css"; 
 
 const containor = createRoot(document.getElementById("root"))
 
-function MyfirstReactComponet() {
+function Header() {
   return (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">AddisElectronics</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-);
-}
-function Home(){
-  return (
-  
-    <h1>Welcome to AddisElectronics</h1>
-)
-}
-
-
-function PartTwo(){
-  return (
-    <div>
-    <header>
+    <>
+     <header className="headerr">
       <img 
       src="ll.jpg"
       alt="React-logo"
-      width="100"
-      height="100" 
+      className="logo"
+      
       />
       
+    
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
     </header>
-    <main>
+    </>
+   
+ 
+);
+}
+function Main(){
+  return (
+    <>
+    <h1>Welcome to AddisElectronics</h1>
+    <div>
+    
+  
       <h1>List Of Products</h1>
       <ol>
         <li>Phone</li>
@@ -60,23 +46,31 @@ function PartTwo(){
         <li>Earpod</li>
         <li>TV</li>
       </ol>
-    </main>
+      </div>
+      </>
+     
+)
+}
+
+
+function Footer(){
+  return (
     <footer>
       <p>@ 20xx SurafelMoges2025 All Right Reserved</p>
     </footer>
-    </div>
-  )
-}
+)}
 
 function Apps(){
   return (
     <div>
-      <MyfirstReactComponet />
-      <Home />
-      <PartTwo />
+      <Header />
+      <Main/>
+      <Footer />
+      
     </div>
     
   );
 }
 
-containor.render(<Apps />);
+containor.render(
+<Apps />);
